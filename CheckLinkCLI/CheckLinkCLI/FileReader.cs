d0598509689756a5a3b95c723187d138f6573fe4
abuZayed15 <abuZayed15>
2020-09-15 02:05:19 -0400

    Created new class files as test

---
+++ CheckLinkCLI/CheckLinkCLI/FileReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CheckLinkCLI
+{
+    public class FileReader
+    {
+        public string fileName { get; set; }
+        public FileReader(string file)
+        {
+            fileName = file;
+
+        }
+
+        //public void ReadFileContent(string file)
+        //{
+        //    var content = new FileStream()
+        //}
+    }
+}
---
+++ CheckLinkCLI/CheckLinkCLI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CheckLinkCLI
+{
+    public class Program
+    {
+        public static string file;
+        FileReader FileReader = new FileReader(file);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
