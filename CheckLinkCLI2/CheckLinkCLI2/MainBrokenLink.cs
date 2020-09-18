622dd3ed633b965154917c179e10c239cbacba49
abuZayed15 <abuZayed15>
2020-09-18 01:34:08 -0400

    added if check for dev and prod environment

--- CheckLinkCLI2/CheckLinkCLI2/MainBrokenLink.cs
+++ CheckLinkCLI2/CheckLinkCLI2/MainBrokenLink.cs
@@ -14,37 +14,57 @@ namespace CheckLinkCLI2
 {
     public class MainBrokenLink
     {
-        private static readonly string linkFile = @"D:\Documents\Seneca College\OSD600\check-link\check-link\CheckLinkCLI2\CheckLinkCLI2\links.txt";
-
-        static void Main(string[] args)
+        #region Debug or Release
+        public static bool IsDebug()
         {
-            //var statusCode = (int)MainBrokenLink.GetHttpStatusCode("https://duckduckgo.m");
-            //var statusCode = GetAllEndPointWithUri("https://duckduckgo.m");
-            //Console.WriteLine($"The status code of the link is {GetAllEndPointWithUri("https://duckduckgo.com")}");
+#if (DEBUG)
+            return true;
+#else
+            return false;
+#endif
+        }
+
+        #endregion
+
+        private static readonly string linkFile = @"D:\Documents\Seneca College\OSD600\check-link\check-link\CheckLinkCLI2\CheckLinkCLI2\urls.txt";
+
+        public static void Main(string[] args)
+        {
             FileReader FileReader = new FileReader();
             WebLinkChecker LinkChecker = new WebLinkChecker();
 
-
-            if (args.Length != 1 || args.Length > 1)
+            if (IsDebug())
             {
-                Console.WriteLine("Please provide file name with links as an argument...");
-            }
-
-            else
-            {
-                var links = FileReader.ExtractLinks(args[0]);
-
-                //var links = FileReader.ExtractLinks(linkFile);
-
+                var links = FileReader.ExtractLinks(linkFile);
                 foreach (var link in links)
                 {
                     LinkChecker.GetAllEndPointWithUri(link);
                 }
             }
 
+            else
+            {
+                if (args.Length != 1 || args.Length > 1)
+                {
+                    Console.WriteLine("Please provide file name with links as an argument...");
+                }
+
+                else
+                {
+                    var links = FileReader.ExtractLinks(args[0]);
+                    foreach (var link in links)
+                    {
+                        LinkChecker.GetAllEndPointWithUri(link);
+                    }
+                }
+            }
 
 
         }
+
+
+
+
 
     }
 
